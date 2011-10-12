882316ab4ca8a117b481ba107e6e2cd13246e8f1
naokis <naokis@ranadesign.com>
2011-10-12 11:34:24 +0900

    Initial commit (Ver.1.1)

---
+++ README.markdown
@@ -0,0 +1,128 @@
+#&quot;twTicker&quot; jQuery plugin
+
+##Description
+このスクリプトはjQueryプラグインです。
+指定したエリア内に横スライドするだけのシンプルなTwitterティッカーを表示します。
+
+---
+##Demos
+http://kaelab.ranadesign.com/blog/demo/twTicker/
+
+---
+##Usage
+
+###Step01
+head要素内で jquery.twTicker.jsと、jquery.twTicker.cssをそれぞれ読み込みます。もちろん、jquery.jsを読み込んだ後に読み込みましょう。
+
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript" src="jquery.twTicker.js"></script>
+	<link rel="stylesheet" type="text/css" href="jquery.twTicker.css" />
+
+###Step02
+Step01の通りスクリプトファイルを読み込んだ後に、以下の例のように実行します。
+ティッカーのコンテナとなる要素をjQueryセレクタで指定して、実行します。引数でオプションを指定できます。
+
+	<script type="text/javascript">
+	$(function() {
+		$('#twTicker').twTicker({
+			// some options...
+			query:'from%3Atoptweets_ja',
+			length: 10
+		});
+	});
+	</script>
+	...
+	<div id="twTicker"></div>
+
+オプションの一覧は次の表の通りです。
+
+<table border="1">
+<colgroup span="1" class="colh">
+<colgroup span="1" class="colh">
+<colgroup span="1" class="cold">
+<thead>
+<tr>
+<th>オプション名<br>(option name)</th>
+<th>デフォルト値<br>(default value)</th>
+<th>備考<br>(note)</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>query</td>
+<td>'from%3Atoptweets_ja'</td>
+<td>Twitter APIに投げるクエリ<br>
+例：'TagName'タグを検索 -&gt; %23TagName<br>
+'UserName'ユーザーを検索 -&gt; from%3AUserName</td>
+</tr>
+<tr>
+<td>length</td>
+<td>10</td>
+<td>Tweetを取得する数</td>
+</tr>
+<tr>
+<td>tweetLink</td>
+<td>true</td>
+<td>Tweetにパーマリンクを張るかどうかを示す論理値</td>
+</tr>
+<tr>
+<td>duration</td>
+<td>20000</td>
+<td>アニメーションにかける時間。多いとスピードが遅くなります。</td>
+</tr>
+<tr>
+<td>loading</td>
+<td>true</td>
+<td>ローディング画像を表示するかどうかを示す論理値</td>
+</tr>
+<tr>
+<td>loadingImg</td>
+<td>'loading.gif'</td>
+<td>ローディング画像のパス</td>
+</tr>
+<tr>
+<td>loadingImgId</td>
+<td>'twTickerLoadingImg'</td>
+<td>ローディング画像のid属性</td>
+</tr>
+<tr>
+<td>className</td>
+<td>'twTicker'</td>
+<td>jQueryセレクタで指定した要素に追加するclass属性。
+デフォルト値から変更する場合は<span class="file">jquery.twTicker.css</span>内の指定も合わせて変更する必要があります。</td>
+</tr>
+<tr>
+<td>wrapperClassName</td>
+<td>'twTickerWrapper'</td>
+<td>Tweet全体を包むラッパー要素のclass属性。
+デフォルト値から変更する場合は<span class="file">jquery.twTicker.css</span>内の指定も合わせて変更する必要があります。</td>
+</tr>
+<tr>
+<td>datePosition</td>
+<td>'after'</td>
+<td>日付をTweetテキストの前につけるか、後ろにつけるかを示す文字列。'before'で前に、それ以外を指定すると後ろにつきます。</td>
+</tr>
+<tr>
+<td>dateFormat</td>
+<td>
+
+	function(y,m,d,hs,ms,ss) {
+	  return " (" + y + "-" + m
+		+ "-" + d + " " + hs+ ":"
+		+ ms + ":" + ss + ")";
+	}
+
+</td>
+<td>日付表示のフォーマットを返す関数。引数に 年/月/日/時/分/秒 を順番に6つ受け取るのでそれを使って適当な文字列を返すようにします。</td>
+</tr>
+</tbody>
+</table>
+
+---
+##License
+<a href="http://www.opensource.org/licenses/mit-license.html">MIT License</a>
+参考: <a href="https://secure.wikimedia.org/wikipedia/ja/wiki/MIT_License">MIT License - Wikipedia</a>
+
+---
+##Contact
+<a href="http://kaelab.ranadesign.com/blog/2010/09/twticker.html">シンプルなTwitterティッカーを表示するjQueryプラグイン | かえラボBlog</a>
---
+++ jquery.twTicker.js
@@ -0,0 +1,206 @@
+/**
+ * "twTicker" jQuery plugin
+ *
+ * Simple Twitter slideshow.
+ *
+ * @author     RaNa design associates, inc.
+ * @copyright  2010 RaNa design associates, inc.
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    Release: 1.1
+ * @since      2010-09-16
+ * @update     2011-10-12
+ */
+
+(function($) {
+	$.fn.twTicker = function(options) {
+		if (!this[0]) { return; }
+		// Variable to keep index of instance
+		if (!$.twTicker) { $.twTicker = {}; }
+		if (!$.twTicker.index) { $.twTicker.index = 0; }
+
+		this.each(function(i) {
+			var index = $.twTicker.index;
+			var instanceName = "instance" + (index + i);
+			// Make new instance in $.twTicker array object
+			$.twTicker[instanceName] = new $.TwTicker();
+			// Merge options from arguments
+			var opt = $.twTicker[instanceName].options;
+			$.extend(opt, options);
+			// Append wrapper element
+			$.twTicker[instanceName].wrapper = $('<div/>', {
+					"class": opt.wrapperClassName
+				}).appendTo(this);
+			// Insert loading image
+			$.twTicker[instanceName].showLoadingImg();
+			// Insert Twitter script element
+			$('<script/>', {
+				"src": ("https:" === document.location.protocol ? "https://" : "http://") + "search.twitter.com/search.json?q=" + opt.query + "&rpp=" + opt.length + "&callback=jQuery.twTicker.instance" + (index + i) + ".callback"
+			}).appendTo("head");
+			// Update index of instance
+			$.twTicker.index ++;
+			// Setting for style
+			$(this).addClass(opt.className);
+		});
+		
+		return this;
+	};
+
+	// TwTicker Constructor
+	$.TwTicker = function() {
+		this.options = {
+			query: "from%3Atoptweets_ja",
+			// Query Example
+			//   %23TagName
+			//   from%3AUserName
+			length: 5,
+			tweetLink: true,
+			duration: 20000,
+			loading: true,
+			loadingImg: "loading.gif",
+			loadingImgId: "twTickerLoadingImg",
+			className: "twTicker",
+			wrapperClassName: "twTickerWrapper",
+			datePosition: "after",
+			dateFormat: function(y, m, d, hs, ms ,ss) {
+				return " (" + y + "-" + m + "-" + d + " " + hs + ":" + ms + ":" + ss + ")";
+			}
+		};
+		this.wrapper = {};
+	};
+
+	$.TwTicker.prototype = {
+		callback: function(data) {
+			var self = this,
+				opt = this.options,
+				res = data.results,
+				resLength = data.results.length,
+				tweetContainers = [];
+			this.hideLoadingImg();
+			// Prevent query error
+			if (!res[0]) { return; }
+
+			// Build Tweet List
+			var $twTickerInner = $('<div/>', {"class":"twTickerInner"});
+			$.each(res, function(i) {
+				var resUser = res[i].from_user,
+					resText = res[i].text,
+					resDate = res[i].created_at,
+					resId = res[i].id_str,
+					content = "";
+				// Join text data
+				content =  resUser + ": " + resText;
+				if (opt.datePosition === "before") {
+					content = self.createDateText(resDate) + content
+				} else {
+					content += self.createDateText(resDate)
+				}
+				content = self.htmlspecialchars(content);
+				// Set content link
+				if (opt.tweetLink) {
+					content = $('<a/>', {
+							"href": "http://twitter.com/" + resUser + "/status/" + resId,
+							"target": "_blank"
+						}).html(content);
+				}
+				// Tweet container
+				tweetContainers[i] = $('<div/>', {"class":"tweet"});
+				// Publish
+				tweetContainers[i].append(content).appendTo($twTickerInner);
+			});
+
+			// Copy Tweet List and append to wrapper
+			var $twTickerInner2 = $twTickerInner.clone();
+			$(this.wrapper).append($twTickerInner).append($twTickerInner2);
+
+			// Constant variables in animation function
+			var boxIndex = 0,
+				baseDuration = opt.duration * resLength;
+			// Animation function
+			function animateTweet() {
+				// Animation target box is alternately changed
+				var $box = (boxIndex === 0) ? $twTickerInner : $twTickerInner2;
+				var boxWidth = $box.width(),
+					boxMarginL = $box.css("marginLeft");
+
+				boxMarginL = boxMarginL.slice(0, boxMarginL.indexOf("px"));
+				boxMarginL = parseInt(boxMarginL) || 0;
+				// Compute remaining distance
+				var distance = (boxMarginL === 0) ?
+					boxWidth : boxWidth + boxMarginL;
+				// Compute animation duration
+				var duration = Math.floor(baseDuration * (distance / boxWidth));
+				// Animation
+				$box.animate({
+					"marginLeft": "-=" + distance + "px"
+				}, duration, "linear", function() {
+					// Change target box
+					boxIndex = (boxIndex === 0) ? 1 : 0;
+					$box.appendTo(self.wrapper).css({"marginLeft": 0});
+					// Recursive Processing
+					animateTweet();
+				});
+			}
+			// Initial animation
+			animateTweet();
+			// If hover to tweet wrapper, stop the animation.
+			this.wrapper.hover(function() {
+				$twTickerInner.stop();
+				$twTickerInner2.stop();
+			}, function() {
+				animateTweet();
+			});
+		},
+		showLoadingImg: function() {
+			var opt = this.options;
+			if (opt.loading) {
+				var $loadingImg = $('<img/>', {
+					"src": opt.loadingImg,
+					"id": opt.loadingImgId
+				}).appendTo(this.wrapper);
+			}
+		},
+		hideLoadingImg: function() {
+			var opt = this.options;
+			if (opt.loading) {
+				$("#" + opt.loadingImgId).remove();
+			}
+		},
+		createDateText: function(responseDateText) {
+			// ResponseDateText example:
+			// ["Fri,", "17", "Sep", "2010", "06:28:39", "+0000"]
+			var created_at = responseDateText.split(" ");
+				// Date string -> Date object
+			var d = new Date(created_at[2] + " " + created_at[1] + ", " + created_at[3] + ", " + created_at[4]);
+				// UTC -> JST (+9h)
+			d.setHours(d.getHours() + 9);
+				// If hour/minutes in single digit, add "0" to it's double digit
+			var dY = d.getFullYear(),
+				dM = d.getMonth() + 1,
+				dD = d.getDate(),
+				dHs = (d.getHours() + 100).toString().slice(1, 3),
+				dMs = (d.getMinutes() + 100).toString().slice(1, 3),
+				dSs = (d.getSeconds() + 100).toString().slice(1, 3);
+			return this.options.dateFormat(dY,dM,dD,dHs,dMs,dSs);
+		},
+		htmlspecialchars: function(s) {
+			s = s.replace(/&/g,"&amp;").replace(/&amp;amp;/g,"&amp;").replace(/"/g,"&quot;").replace(/'/g,"&#039;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
+			return s;
+		},
+
+		// Below this line, inactive method.
+		createUserLink: function(text) {
+			text = '<a href="http://twitter.com/' + text + '" target="_blank">' + text + '</a>';
+			return text;
+		},
+		createReplyLink: function(text) {
+			text = text.replace(/@([\w_]+)/g, '<a href="http://twitter.com/$1" target="_blank">@$1</a>');
+			return text;
+		},
+		createTagLink: function(text) {
+			text = text.replace(/#([\w_]+)/g, '<a href="http://twitter.com/#search?q=%23$1" target="_blank">#$1</a>');
+			return text;
+		}
+	};
+
+})(jQuery);
+
